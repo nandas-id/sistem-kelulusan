a485cf160221d6047759bf81d17cfa3a20fbdd6d
nandas-id <nandaariansyah23@admin.smp.belajar.id>
2025-06-02 10:53:56 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const kelulusanSettings = {
   tahunAjaran: "2024/2025",
-  tanggal: "2025-06-02T22:00:00+07:00", // ganti tanggal dan jam sesuai kebutuhan
+  tanggal: "2025-06-02T08:00:00+07:00", // ganti tanggal dan jam sesuai kebutuhan
   aktif: true
 };
 
